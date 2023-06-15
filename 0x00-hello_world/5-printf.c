2e30c691470bb911077f35a30492581565ed9bc7
snerax <snera48@hotmail.com>
2023-06-15 10:48:22 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,7 +10,11 @@
  */
 int main(void)
 {
-	printf("%s%s%s%s%s\n", "with proper grammar, but the outcome", " is a", " piece", " of", " art,");
+	printf("%s%s%s%s%s\n",
+	       "with proper grammar, but the outcome",
+	       " is a",
+	       " piece",
+	       " of",
+	       " art,");
 	return (0);
 }
-
