66f5d9d8dac9e183fc77b8acf28e8cd963d1e70c
snerax <snera48@hotmail.com>
2023-06-15 11:23:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#define STDERR_FILENO 2
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the specified message to the standard error
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = sizeof(message) - 1;
+	ssize_t ret;
+
+	ret = syscall(SYS_write, STDERR_FILENO, message, len);
+	if (ret != (ssize_t)len)
+		return 1;
+
+	return 1;
+}
