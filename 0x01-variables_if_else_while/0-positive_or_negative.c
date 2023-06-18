e1bb82db9d808a7e8cda29b07402ac984afff142
snerax <snera48@hotmail.com>
2023-06-18 13:02:24 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,15 +3,22 @@
 #include <ctime>
 
 int main() {
-  srand(time(0));
-  int n = rand() - RAND_MAX / 2;
-  std::cout << n << " is ";
-  if (n > 0) {
-    std::cout << "positive";
-  } else if (n == 0) {
-    std::cout << "zero";
-  } else {
-    std::cout << "negative";
-  }
-  std::cout << std::endl;
+    srand(time(0)); // Seed the random number generator
+
+    int n = rand() % RAND_MAX; // Generate a random number
+
+    std::cout << "The number " << n << " is ";
+
+    if (n > 0) {
+        std::cout << "positive";
+    } else if (n == 0) {
+        std::cout << "zero";
+    } else {
+        std::cout << "negative";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
 }
