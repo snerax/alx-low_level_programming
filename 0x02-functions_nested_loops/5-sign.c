047b74aa70a45c50b2b134f694caede2ffd3f48a
snerax <snera48@hotmail.com>
2023-06-21 10:23:27 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-
-int print_sign(int n) {
-    if (n > 0) {
-        putchar('+');
-        return 1;
-    } else if (n == 0) {
-        putchar('0');
-        return 0;
-    } else {
-        putchar('-');
-        return -1;
-    }
-}
+#include "sign.h"
 
 int main() {
     int num = 10;
@@ -28,3 +16,4 @@ int main() {
 
     return 0;
 }
+
