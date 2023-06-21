49b9a4ab00e6fe5776a697f4dff736300a660b11
snerax <snera48@hotmail.com>
2023-06-21 08:54:34 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,26 @@
-include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_putchar - Prints _putchar followed by a new line.
+ * print_putchar - Prints _putchar followed by a new line using nested while loops.
  */
 void print_putchar(void)
 {
-    printf("_putchar\n");
+    int outer_counter = 0;
+
+    while (outer_counter < 5)
+    {
+        int inner_counter = 0;
+
+        while (inner_counter < 5)
+        {
+            putchar('_');
+            inner_counter++;
+        }
+
+        putchar('\n');
+        outer_counter++;
+    }
 }
 
 /**
@@ -18,3 +32,4 @@ int main(void)
 {
     print_putchar();
     return 0;
+}
